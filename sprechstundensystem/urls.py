636182b8cdd3ce8ce04769277c4f9144aaa5844e
Michael Loipführer <michael.loipfuehrer@stusta.de>
2020-08-10 21:30:09 +0200

    disable django admin in non debug

--- sprechstundensystem/urls.py
+++ sprechstundensystem/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('management.urls')),
-    path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('admin/', admin.site.urls),
+    ]
